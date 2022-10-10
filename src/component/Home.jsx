0bd33bf275180d22e29bf2b99b44fb896727d2f1
arjunglal <arjunglal.me@gmail.com>
2022-10-10 10:48:30 +0530

    typing

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import {HiArrowNarrowRight} from 'react-icons/hi'
+import Typical from 'react-typical'
 const Home = () => {
   return (
     <div name='home' className='w-full h-screen bg-[#01192f]'>
@@ -7,7 +8,10 @@ const Home = () => {
         <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
             <p className='text-pink-600'>Hi there, I'm</p>
             <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Arjun G Lal</h1>
-            <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>Front End Developer.</h2>
+            <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0] whitespace-nowrap'>
+              <Typical loop={Infinity} wrapper='p' steps={['Front End Developer.',2000,' Front End Developer.',2000]}/>
+                
+            </h2>
             <p className='text-[#8892b0] py-4 max-w-[700px]'> A tech enthusiast with experience in Web development and coding who is eager to learn, grasp and acquire more skills that the tech world can offer.</p>
             <div>
                 <button className='group text-white border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600 duration-300'>View Work
